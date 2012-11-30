579e4f4a3d369b2aa2c58eb87c00b5a78a6ce829
Jon Morton <jonanin@gmail.com>
2012-11-29 19:53:22 -0800

    add complete dash view

--- hackpack/views.py
+++ hackpack/views.py
@@ -1,28 +1,29 @@
 
 from app import app
 from models import *
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 from auth import auth
 from wtfpeewee.orm import model_form
+from util import get_object_or_404
+import datetime
+
 
 @app.route("/")
 @auth.login_required
 def dash_index():
 	return render_template("index.html")
 
-@app.route("/dash-past")
-def dash_past():
-	return render_template("dash-past.html")
+@app.route("/hackathon/<int:hackathon_id>")
+def dash(hackathon_id):
+	hackathon = get_object_or_404(Hackathon, id = hackathon_id)
+	now = datetime.datetime.now()
 
-@app.route("/dash-present")
-def dash_present():
-	return render_template("dash-present.html")
-
-@app.route("/dash-future")
-def dash_future():
-	return render_template("dash-future.html")
-
-
+	if now < hackathon.start_date:
+		return render_template("dash-future.html", hackathon = hackathon)
+	elif now < hackathon.end_date:
+		return render_template("dash-present.html", hackathon = hackathon)
+	else:
+		return render_template("dash-past.html", hackathon = hackathon)
 
 HackathonForm = model_form(Hackathon, exclude=("facebook_id", "owner"))
 
@@ -37,6 +38,7 @@ def hackathon_create():
 			form.populate_obj(hack)
 			hack.owner = auth.get_logged_in_user()
 			hack.save()
+			return redirect(url_for("dash", hackathon_id = hack.id))
 	else:
 		form = HackathonForm()
 
